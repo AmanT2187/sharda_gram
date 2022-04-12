373005a6639135ce707118fc24b428b22c2c5e15
Aman2187 <2021002187.aman@ug.sharda.ac.in>
2022-04-12 13:08:12 +0530

    firebase_done

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,11 @@ void main() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
       options: const FirebaseOptions(
-          apiKey: "AIzaSyCZ-xrXqD5D19Snauto-Fx_nLD7PLrBXGM",
-          appId: "1:585119731880:web:eca6e4b3c42a755cee329d",
-          messagingSenderId: "585119731880",
-          projectId: "instagram-clone-4cea4",
-          storageBucket: 'instagram-clone-4cea4.appspot.com'),
+          apiKey: "AIzaSyB0I5F1dhS4Lm_bxxhGnkjcIv0i2bvcbVo",
+          appId: "1:739980343051:web:73175010d92800daea940e",
+          messagingSenderId: "739980343051",
+          projectId: "sharda-gram",
+          storageBucket: 'sharda-gram.appspot.com'),
     );
   } else {
     await Firebase.initializeApp();
